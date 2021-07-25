1c60d63cfa09a091de8faeb82ddc38cd3d507da6
bocon <tan.truong@go2joy.vn>
2021-07-25 20:03:34 +0700

    Complete writer

--- src/app.py
+++ src/app.py
@@ -1,25 +1,46 @@
 import os
 import itertools
+import logging
+import json
+from contextlib import ExitStack
 
 from marshmallow import ValidationError
 
 from src.validator import Validator
 from src.utils import read_file
+from src.utils import open_file
 from src.utils import LANGUAGE_NT
 
-current_dir = os.path.abspath(os.getcwd())
-language_file = os.path.join(current_dir, 'data/raw.csv')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 validator = Validator()
 
 
-for line in itertools.islice(read_file(language_file), 50):
+current_dir = os.path.abspath(os.getcwd())
+language_file = os.path.join(current_dir, 'data/raw.csv')
+en_file = os.path.join(current_dir, 'output/en.json')
+vi_file = os.path.join(current_dir, 'output/vi.json')
+
+
+dict_en = dict()
+dict_vi = dict()
+
+
+for line in itertools.islice(read_file(language_file), 3):
     temp = validator.dump(LANGUAGE_NT(*line))
     try:
         obj = validator.load(temp)
-        print(obj)
-    except ValidationError:
-        print('error')
-    finally:
-        continue
+        dict_en.update({obj.key: obj.eng})
+        dict_vi.update({obj.key: obj.vn})
+    except ValidationError as e:
+        logger.warning(e.messages)
+
+
+with ExitStack() as stack:
+    f_en = stack.enter_context(open_file(en_file))
+    f_vi = stack.enter_context(open_file(vi_file))
+    json.dump(dict_vi, f_vi, ensure_ascii=False, indent=4)
+    json.dump(dict_en, f_en, ensure_ascii=False, indent=4)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import csv
 from collections import namedtuple
+from contextlib import contextmanager
 
 
 def read_file(file_name):
@@ -11,6 +12,17 @@ def read_file(file_name):
         # Skip the header
         next(reader)
         yield from reader
+
+
+@contextmanager
+def open_file(f_name):
+    print(f'opening {f_name}')
+    f = open(f_name, 'a')
+    try:
+        yield f
+    finally:
+        print(f'closing {f_name}')
+        f.close()
 
 
 LANGUAGE_NT = namedtuple('Language', 'english vietnamese remarks')
